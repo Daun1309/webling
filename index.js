85021c0bf2cd061c6f182e8cf3f936b775a6e2df
Daun1309 <100025580+Daun1309@users.noreply.github.com>
2022-12-13 10:00:12 +0900

    js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ $('body').prepend('<header>');
 $('body').append('<footer>');
 
 $('header').load('./inc.html header', head);
-$('footer').load('./inc.html footer > div');
+$('footer').load('./inc.html footer > div'); 
 
 let idx = localStorage.idx || 0;  
 
@@ -10,10 +10,17 @@ function head(){
     $('header ul li').eq(idx).find('a').addClass('active');
     //$('header ul li').eq(idx).find('a').css('color','pink');
 
+    test();
     console.log(idx);
 
     $('header ul li').click(function(){
         let idx = $(this).index();
         localStorage.idx = idx;
     })
+}
+
+function test(){
+    if(window.location.href.split('/')[3] == 'index.html'){
+        $('header ul li').eq(idx).find('a').removeClass('active');
+    }
 }
