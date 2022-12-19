d07713f4cfd58b0acbc336b16d129c86d98f18e7
Daun1309 <100025580+Daun1309@users.noreply.github.com>
2022-12-19 15:34:30 +0900

    header

--- index.js
+++ index.js
@@ -27,3 +27,39 @@ function test(){
     }
 }
 
+let didScroll;
+let lastScrollTop = 0;
+let delta = 5;
+let navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    let st = $(this).scrollTop();
+    
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('nav-up');
+        console.log("addClass");
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+            console.log("removeClass");
+        }
+    }
+    
+    lastScrollTop = st;
+}
